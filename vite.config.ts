6e95d63591adecbe177268465b3480d97c5eea55
FelipeDesigne <felipebdias98@gmail.com>
2024-11-22 00:30:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ChatBot/', // substitua pelo nome do seu repositório no GitHub
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: '/ChatBot/',  // Defina o caminho base para o GitHub Pages
 });
